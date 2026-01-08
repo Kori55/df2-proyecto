77136db3899178e9e65aff8f53e3786506db70c2
Kori <169088148+Kori55@users.noreply.github.com>
2026-01-08 18:44:15 -0300

    creacion archivos principales
    aaaaaasnkjvkjvj

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const nombre = document.getElementById("nombre")
+const mail = document.getElementById("mail")
+const contrasena = document.getElementById("contrasena")
+const codigo = document.getElementById("codigo")
