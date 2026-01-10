67570deef84658c93603847202436d22e792160a
Kori3 <sebast.sepulvedag@duocuc.cl>
2026-01-10 19:04:58 -0300

    implementacion pagina iniciar sesion
    si lo copié 1x1 pero es pq tengo prisa ahora lo puedo decorar y hacer mas bonito mañana :>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
 const menu = document.getElementById("menu")
 
-const nombre = document.getElementById("nombre")
-const mail = document.getElementById("mail")
-const contrasena = document.getElementById("contrasena")
-const codigo = document.getElementById("codigo")
-
 function toggle() {
     menu.classList.toggle("no")
 }
+
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+
+const form = document.getElementById("form")
+const parrafo = document.getElementById("errores")
+
+form.addEventListener("submit", e=> {
+    e.preventDefault()
+    let errores = ""
+    let entrar = false
+    let expCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\.\w{2,4})+$/
+
+    if (nombre.value.length < 6) {
+        // alert("Nombre muy corto al menos 6 letras")
+        errores += 'Nombre corto al menos 6 letras <br>'
+        entrar = true
+    }
+
+    if (!expCorreo.test(email.value)){
+        errores += 'correo no valido <br>'
+        entrar = true
+    }
+
+    if (pass.value.length < 8) {
+        errores += 'contraseña al menos 8 caracteres <br>'
+        entrar = true
+    }
+
+    if (entrar) {
+        parrafo.innerHTML = errores
+    }
+})
