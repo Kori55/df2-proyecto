3b2f7f68968dc0a9610f12b674d0fc750272365e
Kori3 <sebast.sepulvedag@duocuc.cl>
2026-01-10 18:25:29 -0300

    boton que activa barra lateral (ta fea)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
+const menu = document.getElementById("menu")
+
 const nombre = document.getElementById("nombre")
 const mail = document.getElementById("mail")
 const contrasena = document.getElementById("contrasena")
 const codigo = document.getElementById("codigo")
+
+function toggle() {
+    menu.classList.toggle("no")
+}
